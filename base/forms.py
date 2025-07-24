201c899ca487f7eaf64d6f0773701d37e95b441a
1024-mb <m.sajjad.2007.jan@gmail.com>
2025-07-24 13:18:26 +0800

    home filtering system for products

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
 from base.models import Review, User
+from django.contrib.auth.forms import UserCreationForm
 
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = '__all__'
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username']
 
--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from .models import Clothing, User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.db.models import Q
 from django.contrib import messages
 
@@ -14,7 +14,7 @@ def login_page(request):
 
     if request.method == "POST":
         try:
-            username = request.POST.get('username')
+            username = request.POST.get('username').lower()
             password = request.POST.get('password')
 
             UserChecked = User.objects.get(username=username)
@@ -47,41 +47,127 @@ def login_page(request):
         return render(request, 'login_register.html', context)
 
 def register(request):
-    signup_form = UserCreationForm()
-    if request.method == "POST":
-        username = request.GET.get('username')
-        password = request.GET.get('password')
-        email = request.GET.get('email')
-        firstname = request.GET.get('firstname')
-        firstname = request.GET.get('lastname')
+    signup_form = SignUpForm()
 
-        try:
-            userchecked = User.objects.get(username=username)
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
         
-        except User.DoesNotExist:
-            pass
+        if form.is_valid():
+            if '@' not in request.POST.get('email'):
+                raise form.ValidationError('email is invalid')
+            
+            if len(request.POST.get('username')) > 150:
+                raise form.ValidationError('username is too long')
+            
+
+            try:
+                userchecked = User.objects.get(username=request.POST.get('username'))
+                raise form.ValidationError('username is already in use')
+            
+            except User.DoesNotExist:
+                user = form.save(commit=False)
+                user.username = user.username.lower()
+                user.save()
+
+                login(request, user)
+                messages.success(request, 'Account created')
+
+                return redirect('home')
+
+        else:
+            if '@' not in request.POST.get('email'):
+                messages.error(request, 'Please enter a valid email address')
+                context = {'operation': 'Signup',
+                        'form': signup_form}
+                
+                return render(request, 'login_register.html', context)
+            
+            if len(request.POST.get('username')) > 150:
+                messages.error(request, 'Username is greater than 150 characters')
+
+                context = {'operation': 'Signup',
+                        'form': signup_form}
+                
+                return render(request, 'login_register.html', context)
+            
+            try:
+                userchecked = User.objects.get(username=request.POST.get('username'))
+                messages.error(request, 'Username is already taken')
+                
+                context = {'operation': 'Signup',
+                        'form': signup_form}
+                
+                return render(request, 'login_register.html', context)
+            
+            except:
+                pass
+
+            context = {'operation': 'Signup',
+                    'form': signup_form}
+            
+            return render(request, 'login_register.html', context)
 
 
     else:
-        context = {'operation': 'Signup'}
+        context = {'operation': 'Signup',
+                   'form': signup_form}
+        
         return render(request, 'login_register.html', context)
 
 # Create your views here.
 def home(request):
+    order = '-rating'
+    if request.method == 'POST':
+        order = request.POST.get('order')
+        print(order)
+        
+        if order == 'new_to_old':
+            order = '-created'
+            print('126')
+
+        elif order == 'old_to_new':
+            order = 'created'
+            print('130')        
+
+        elif order == 'low_to_high':
+            order = 'price'
+            print('134')
+
+        elif order == 'high_to_low':
+            order = '-price'
+            print('138')
+
+        elif order == 'top_rated':
+            order = '-rating'
+            print('142')
+
     q = request.GET.get('q')
 
+    men_categories = ['T-Shirts & Tops', 'Jerseys', 'Hoodies',
+                    'Sweatshirts and Tracksuits', 'Pants', 'Tights', 'Shorts',
+                    'Sportswear']
+    
+    women_categories = ['T-Shirts & Tops', 'Jerseys', 'Hoodies',
+                        'Sweatshirts and Tracksuits', 'Sports Bras', 'Jerseys',
+                        'Hoodies', 'Jackets', 'Pants', 'Tights', 'Shorts',
+                        'Sportswear',]
+    
+    children_categories = ['New Arrivals', 'Boys Clothing', 'Girls Clothing',
+                            'Boys Shoes', 'Girls Shoes']
+
     if q != None:
-        all_categories = Clothing.objects.order_by('-rating')
         extracted = Clothing.objects.filter(
             Q(category__icontains=q) |
             Q(name__icontains=q) |
             Q(description__icontains=q) |
             Q(category__icontains=q) |
-            Q(brand__icontains=q)).order_by('-rating')
+            Q(brand__icontains=q)).order_by(order)
 
 
         context = {'clothing': extracted,
-                   'categories': all_categories,
+                   'mens_categories': men_categories,
+                   'womens_categories': women_categories,
+                   'child_categories': children_categories,
                    'query': q,
                    'result_count': extracted.count()}
 
@@ -89,15 +175,21 @@ def home(request):
         return render(request, 'base/home.html', context)
     
     else:
-        extracted = Clothing.objects.order_by('-rating')
+        extracted = Clothing.objects.order_by(order)
 
         context = {'clothing': extracted,
-                   'categories': extracted}
+                    'mens_categories': men_categories,
+                    'womens_categories': women_categories,
+                    'child_categories': children_categories}
 
         return render(request, 'base/home.html', context)
 
 @login_required(login_url='login')
 def Logout(request):
-    logout(request)
-    return redirect('home')
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('home')
+
+    else:
+        return redirect('home')
 
