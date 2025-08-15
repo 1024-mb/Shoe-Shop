2a82494d03d77eff4a3e5ab36eda80aa9832d6fc
1024-mb <m.sajjad.2007.jan@gmail.com>
2025-08-15 23:34:24 +0800

    fixed lots of styling issues

--- base/views.py
+++ base/views.py
@@ -33,6 +33,41 @@ import random
 
 stripe.api_key = os.getenv('stripekey')
 
+def send_otp(first_name, otp, usr_email, subject):
+    receipt = EmailMessage()
+    receipt['Subject'] = subject
+    receipt['From'] = 'm.sajjad.2007.jan@gmail.com'
+    receipt['To'] = usr_email
+    
+    receipt.set_content('Email follows')
+
+    emailText = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <style>
+        
+        </style>
+    </head>
+
+    <body>
+        <h2>{subject}</h2>
+        <p>Dear {first_name},
+        Your OTP is: {otp}. This OTP is valid
+        for 15 minutes.
+        
+        </p>
+    </body>
+
+    </html>
+
+    """
+
+    receipt.add_alternative(emailText, subtype='html')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login('m.sajjad.2007.jan@gmail.com', 'yufr nluw qvwy jaid')
+        smtp.send_message(receipt)
 
 def set_review(id_product):
     Reviews = Review.objects.filter(product_ID=id_product)
@@ -54,58 +89,63 @@ def set_review(id_product):
     Product.save()
 
 def login_page(request):
-    next_url = request.GET.get('next')
+    if not(request.user.is_authenticated):
+        next_url = request.GET.get('next')
 
-    if request.method == "POST":
-        try:
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            UserChecked = User.objects.get(username__exact=username)
-
-            user = authenticate(request, username=username, password=password)
-
+        if request.method == "POST":
             try:
-                if UserChecked.is_active == True:
-                    login(request, user)
+                username = request.POST.get('username')
+                password = request.POST.get('password')
 
-                    if next_url:
-                        return redirect(next_url)
-                    
+                UserChecked = User.objects.get(username__exact=username)
+
+                user = authenticate(request, username=username, password=password)
+
+                try:
+                    if UserChecked.is_active == True:
+                        login(request, user)
+
+                        if next_url:
+                            return redirect(next_url)
+                        
+                        else:
+                            return redirect('home')
+                        
                     else:
-                        return redirect('home')
+                        request.session['user_id'] = UserChecked.id
+                        request.session['first_name'] = UserChecked.first_name
+                        request.session['email'] = UserChecked.email
+
+                        messages.error(request, 'Account setup not fully completed.')
+                        return redirect('confirm_email')
+
+                except AttributeError:
+                    messages.warning(request, "Password is incorrect")
                     
-                else:
-                    request.session['user_id'] = UserChecked.id
-                    request.session['first_name'] = UserChecked.first_name
-                    request.session['email'] = UserChecked.email
-
-                    messages.error(request, 'Account setup not fully completed.')
-                    return redirect('confirm_email')
-
-            except AttributeError:
-                messages.warning(request, "Password is incorrect")
+                    context = {'operation': 'login'}
+                    return render(request, 'login_register.html', context)
                 
+            except User.DoesNotExist:
+                messages.warning(request, "User does not exist")
                 context = {'operation': 'login'}
                 return render(request, 'login_register.html', context)
-            
-        except User.DoesNotExist:
-            messages.warning(request, "User does not exist")
+                
+
+        else:
             context = {'operation': 'login'}
             return render(request, 'login_register.html', context)
-            
-
+        
     else:
-        context = {'operation': 'login'}
-        return render(request, 'login_register.html', context)
+        messages.error(request, 'Already signed in')
+        return redirect('home')
 
 def confirm_email(request):
-    if int(request.session.get('count', 0)) < 7:
+    if int(request.session.get('count', 0)) < 7 and not(request.user.is_authenticated):
         if request.method == 'POST':
-            datetime_object = datetime.strptime(request.session['time'],  '%Y-%m-%d %H:%M:%S.%f')
-            time_elapsed = datetime.now() - datetime_object
-            
-            if request.POST.get('OTP') != '' and time_elapsed.total_seconds() < 900:
+            if request.POST.get('OTP') != '' and (datetime.now() - datetime.strptime(request.session['time'],  '%Y-%m-%d %H:%M:%S.%f')).total_seconds() < 900:
+                datetime_object = datetime.strptime(request.session['time'],  '%Y-%m-%d %H:%M:%S.%f')
+                time_elapsed = datetime.now() - datetime_object
+
                 user_input = str(request.POST.get('OTP'))
                 stored_otp = str(request.session.get('otp'))
 
@@ -119,67 +159,28 @@ def confirm_email(request):
 
                 else:
                     user_created = request.session.get('user_id')
-
                     messages.error(request, 'Incorrect OTP entered')
 
                     request.session['count'] = int(request.session.get('count', 0)) + 1
 
                     return redirect('confirm_email')
 
-            elif time_elapsed.total_seconds() >= 900:
+            elif (datetime.now() - datetime.strptime(request.session['time'],  '%Y-%m-%d %H:%M:%S.%f')).total_seconds() > 900:
                 messages.error(request, 'The OTP has expired')
                 return redirect('confirm_email')
 
             else:
                 otp = str(random.randint(100000, 999999))  # 6-digit OTP
-                first_name = request.POST.get('first_name')
 
-                print(otp)
+                # send_otp(first_name, otp, usr_email)
+                send_otp(request.session.get('first_name'), otp, request.session.get('email'), 'HeatSneakers | Confirm Account')
 
-                receipt = EmailMessage()
-                receipt['Subject'] = 'HeatSneakers | Confirm Account'
-                receipt['From'] = 'm.sajjad.2007.jan@gmail.com'
-                receipt['To'] = request.session['email']
-                first_name = request.session['first_name']
-
-                receipt.set_content('Email follows')
-
-                emailText = f"""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <style>
-                    
-                    </style>
-                </head>
-
-                <body>
-                    <h2>Confirm your email address</h2>
-                    <p>Dear {first_name},
-                    Your OTP is: {otp}. This OTP is valid
-                    for 15 minutes.
-                    
-                    </p>
-                </body>
-
-                </html>
-
-                """
-
-                receipt.add_alternative(emailText, subtype='html')
-
-                with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                    smtp.login('m.sajjad.2007.jan@gmail.com', 'yufr nluw qvwy jaid')
-                    smtp.send_message(receipt)
-
-
-                    time = str(datetime.now())
-                    request.session['time'] = time
+                time = str(datetime.now())
+                request.session['time'] = time
 
 
                 # Optionally store OTP in session or database for verification
                 request.session['otp'] = str(otp)
-
                 request.session['count'] = int(request.session.get('count', 0)) + 1
 
                 return redirect('confirm_email')
@@ -187,46 +188,19 @@ def confirm_email(request):
         else:
             otp = str(random.randint(100000, 999999))  # 6-digit OTP
 
-            receipt = EmailMessage()
-            receipt['Subject'] = 'HeatSneakers | Confirm Account'
-            receipt['From'] = 'm.sajjad.2007.jan@gmail.com'
-            receipt['To'] = request.session['email']
-            first_name = request.session['first_name']
+            send_otp(request.session.get('first_name'), otp, request.session.get('email'), 'HeatSneakers | Confirm Account')
 
-            receipt.set_content('Email follows')
-
-            emailText = f"""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <style></style>
-            </head>
-
-            <body>
-                <h2>Confirm your email address</h2>
-                <p>Dear {first_name},
-                Your OTP is: {otp}. This OTP is valid
-                for 15 minutes.
-                
-                </p>
-            </body>
-
-            </html>
-
-            """
-
-            receipt.add_alternative(emailText, subtype='html')
-
-            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                smtp.login('m.sajjad.2007.jan@gmail.com', 'yufr nluw qvwy jaid')
-                smtp.send_message(receipt)
-                request.session['time'] = str(datetime.now())
-
+            time = str(datetime.now())
+            request.session['time'] = time
 
             # Optionally store OTP in session or database for verification
             request.session['otp'] = otp
-
+            
             return render(request, 'confirm_email.html', context={})
+
+    elif request.user.is_authenticated:
+        messages.error(request, 'Error: user already authenticated')
+        return redirect('home')
 
     else:
         user_created = request.session.get('user_id')
@@ -237,9 +211,8 @@ def confirm_email(request):
         messages.error(request, 'Too many unsuccessful attempts.')
         return redirect('home')
 
-
 def set_password(request):
-    if request.method == "POST":
+    if request.method == "POST" and request.POST.get('password1') != '':
         try:
             password1 = request.POST.get('password1')
             password2 = request.POST.get('password2')
@@ -270,16 +243,14 @@ def set_password(request):
     else:
         return render(request, 'set_password.html', context={})
 
-
 def enter_code(request):
-    if request.method == 'POST' and request.session.get('sentTime', None) != None:
+    if request.method == 'POST' and request.session.get('sentTime', None) != None and request.POST.get('resendBtn') != 'resendOTP':
         code = str(request.POST.get('OTP'))
         otp = str(request.session.get('OTP', None))
         timeStart = datetime.strptime(request.session.get('sentTime', None),  '%Y-%m-%d %H:%M:%S.%f')
         timeElapsed = datetime.now() - timeStart
 
         email = request.session.get('email', None)
-
 
         UserChecked = User.objects.filter(email__exact=email)
         print(UserChecked)
@@ -294,6 +265,13 @@ def enter_code(request):
         else:
             return redirect('login')
 
+    elif request.POST.get('resendBtn') == 'resendOTP':
+        otp = random.randint(100000, 999999)
+        request.session['OTP'] = otp
+
+        send_otp(request.session.get('first_name'), otp, request.session.get('email'), 'HeatSneakers | Reset Password')
+
+        return redirect('enter_code')
 
     else:
         return render(request, 'enter_code.html')
@@ -304,49 +282,16 @@ def reset_password(request):
         username = request.POST.get('username')
         userAssociated = User.objects.filter(email=usr_email, username=username)
 
-        if userAssociated != []:
+        if userAssociated.exists():
             otp = random.randint(100000, 999999)
             request.session['OTP'] = otp
             request.session['email'] = usr_email
             request.session['username'] = username
             request.session['sentTime'] = str(datetime.now())
 
-            for item in userAssociated:
-                receipt = EmailMessage()
-                receipt['Subject'] = 'HeatSneakers | Reset Password'
-                receipt['From'] = 'm.sajjad.2007.jan@gmail.com'
-                receipt['To'] = usr_email
-                first_name = item.first_name
+            first_name = userAssociated[0].first_name
 
-                receipt.set_content('Email follows')
-
-                emailText = f"""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <style>
-                    
-                    </style>
-                </head>
-
-                <body>
-                    <h2>Confirm your email address</h2>
-                    <p>Dear {first_name},
-                    Your OTP is: {otp}. This OTP is valid
-                    for 15 minutes.
-                    
-                    </p>
-                </body>
-
-                </html>
-
-                """
-
-                receipt.add_alternative(emailText, subtype='html')
-
-                with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                    smtp.login('m.sajjad.2007.jan@gmail.com', 'yufr nluw qvwy jaid')
-                    smtp.send_message(receipt)
+            send_otp(first_name, otp, usr_email, 'HeatSneakers | Reset Password')
 
             return redirect('enter_code')
         
@@ -484,7 +429,6 @@ def home(request):
 @login_required(login_url='login')
 def logout_page(request):
     if request.user.is_authenticated:
-
         messages.success(request, 'Logged out')
         logout(request)
         return redirect('home')
